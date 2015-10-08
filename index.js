be2931c8ecae721a9faea79f9532f954c5c09f24
Edgar HIPP <hipp.edg@gmail.com>
2015-10-08 20:28:00 +0200

    Supression code

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 "use strict";
 
 var fs = require("fs");
-
 var content = fs.readFileSync(process.argv[2]).toString();
-
 var sum = 0;
-
 
 content.split("\n").forEach(function (n) {
   if(n=="") {
