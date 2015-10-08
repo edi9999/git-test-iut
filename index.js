e02b1b106a57404f12886a26e105a7904d599463
Edgar HIPP <hipp.edg@gmail.com>
2015-10-08 20:28:05 +0200

    Multiline code

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 "use strict";
 
 var fs = require("fs");
-var lines = fs.readFileSync(process.argv[2]).toString().split("\n");
 var val = 8;
+var lines = fs
+  .readFileSync(process.argv[2])
+  .toString()
+  .split("\n");
+
 
 lines.pop();
 
 var sum = 0;
 
 lines.forEach(function (n) {
-  sum+=parseInt(n, val);
+  sum += parseInt(n, val);
 });
 
 console.log(sum);
